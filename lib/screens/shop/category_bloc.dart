bbc810beb1ee18e1b9bf7f00e33f04d7333a97b4
Mahdi <Mahdimoaz@gmail.com>
2021-12-12 13:26:30 +0330

    change limited product

--- lib/screens/shop/category_bloc.dart
+++ lib/screens/shop/category_bloc.dart
@@ -41,7 +41,7 @@ class CategoryBloc {
 
   String get filter {
     final text =
-        '{"page":{"offset":${_offset * 2},"limit":2},"sort":[{"field":"title","dir":"asc"}],"filters":[[{"field":"category_id","op":"=","value":${_category.value.id}}]]}';
+        '{"page":{"offset":${_offset * 30},"limit":30},"sort":[{"field":"title","dir":"asc"}],"filters":[[{"field":"category_id","op":"=","value":${_category.value.id}}]]}';
     return text;
   }
 
