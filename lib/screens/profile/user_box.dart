aca1b5fd1a23f83922ac90a40592da29a153c548
Hosseini <mohamad.talkhaby@gmail.com>
2021-12-14 14:28:04 +0330

    fix bug

--- lib/screens/profile/user_box.dart
+++ lib/screens/profile/user_box.dart
@@ -58,7 +58,7 @@ class UserBoxState extends ResourcefulState<UserBox> {
               height: 8.h,
               textInputType: TextInputType.text,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.firstName = val),
+              onChanged: (val) => userInfo!.firstName = val,
               value: userInfo!.firstName,
               label: intl.name,
               maxLine: false,
@@ -72,7 +72,7 @@ class UserBoxState extends ResourcefulState<UserBox> {
               height: 8.h,
               textInputType: TextInputType.text,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.lastName = val),
+              onChanged: (val) => userInfo!.lastName = val,
               value: userInfo!.lastName,
               label: intl.lastName,
               maxLine: false,
@@ -86,14 +86,15 @@ class UserBoxState extends ResourcefulState<UserBox> {
               height: 8.h,
               textInputType: TextInputType.emailAddress,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.email = val),
+              onChanged: (val) => userInfo!.email = val,
               value: userInfo!.email,
               label: intl.email,
               maxLine: false,
               enable: true,
               ctx: context,
               action: TextInputAction.next,
-              formatter: FilteringTextInputFormatter.allow(RegExp(r'[0-9a-zA-Z]')),
+              formatter:
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9a-zA-Z]')),
               textDirection: context.textDirectionOfLocale),
           Space(height: 3.h),
           textInput(
@@ -101,7 +102,7 @@ class UserBoxState extends ResourcefulState<UserBox> {
               enable: true,
               textInputType: TextInputType.multiline,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.address?.address = val),
+              onChanged: (val) => userInfo!.address?.address = val,
               value: userInfo!.address?.address,
               label: intl.address,
               maxLine: true,
@@ -114,7 +115,7 @@ class UserBoxState extends ResourcefulState<UserBox> {
               height: 8.h,
               textInputType: TextInputType.number,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.callNumber = val),
+              onChanged: (val) => userInfo!.callNumber = val,
               value: userInfo!.callNumber,
               label: intl.callNumber,
               maxLine: false,
@@ -128,10 +129,7 @@ class UserBoxState extends ResourcefulState<UserBox> {
               height: 8.h,
               textInputType: TextInputType.number,
               validation: (val) {},
-              onChanged: (val) => setState(() {
-               // userInfo!.whatsApp = val;
-                userInfo!.socialMedia![0].pivot!.link = val;
-              }),
+              onChanged: (val) => userInfo!.socialMedia![0].pivot!.link = val,
               value: userInfo!.whatsApp,
               label: intl.whatsApp,
               enable: true,
@@ -146,40 +144,42 @@ class UserBoxState extends ResourcefulState<UserBox> {
               textInputType: TextInputType.text,
               validation: (val) {},
               enable: true,
-              onChanged: (val) => setState(() {
-                userInfo!.socialMedia![2].pivot!.link = val ?? '';
-              }),
+              onChanged: (val) =>
+                  userInfo!.socialMedia![2].pivot!.link = val ?? '',
               value: userInfo!.skype,
               label: intl.skype,
               maxLine: false,
               ctx: context,
               action: TextInputAction.next,
-              formatter: FilteringTextInputFormatter.allow(RegExp(r'[0-9a-zA-Z]')),
+              formatter:
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9a-zA-Z]')),
               textDirection: context.textDirectionOfLocale),
           Space(height: 3.h),
           textInput(
               height: 8.h,
               textInputType: TextInputType.number,
               validation: (val) {},
-              onChanged: (val) => setState(() => userInfo!.address?.zipCode = val),
+              onChanged: (val) => userInfo!.address?.zipCode = val,
               value: userInfo!.address?.zipCode,
               label: intl.zipCode,
               maxLine: true,
               enable: true,
               ctx: context,
-              action: TextInputAction.newline,
+              action: TextInputAction.done,
               textDirection: context.textDirectionOfLocale),
           Space(height: 3.h),
           Directionality(
               textDirection: context.textDirectionOfLocale,
               child: MyDropdown(
                 value: profileBloc.findCountryName(),
-                alignment: context.isRtl ? Alignment.centerRight : Alignment.centerLeft,
+                alignment: context.isRtl
+                    ? Alignment.centerRight
+                    : Alignment.centerLeft,
                 hint: intl.country,
                 list: profileBloc.loginRegisterBloc!.countries,
                 onChange: (dynamic val) => setState(() {
                   profileBloc.userInfo.countryId = val.id;
-                  if(profileBloc.countryName != 'Iran') {
+                  if (profileBloc.countryName != 'Iran') {
                     profileBloc.userInfo.cityId = null;
                     profileBloc.userInfo.provinceId = null;
                   }
@@ -193,7 +193,8 @@ class UserBoxState extends ResourcefulState<UserBox> {
                 textDirection: TextDirection.rtl,
                 child: StreamBuilder(
                   stream: profileBloc.cityProvinceModelStream,
-                  builder: (context, AsyncSnapshot<CityProvinceModel> snapshot) {
+                  builder:
+                      (context, AsyncSnapshot<CityProvinceModel> snapshot) {
                     if (snapshot.data != null) {
                       return MyDropdown(
                         value: profileBloc.findProvincesName(),
@@ -217,27 +218,29 @@ class UserBoxState extends ResourcefulState<UserBox> {
           if (profileBloc.countryName == 'Iran') Space(height: 3.h),
           if (profileBloc.countryName == 'Iran')
             Directionality(
-                textDirection: TextDirection.rtl,
-                child: StreamBuilder(
-                  stream: profileBloc.cityProvinceModelStream,
-                  builder: (context, AsyncSnapshot<CityProvinceModel> snapshot) {
-                    if (snapshot.data != null && profileBloc.userInfo.address != null) {
-                      return MyDropdown(
-                        value: profileBloc.findCityName(),
-                        alignment: Alignment.centerRight,
-                        hint: intl.city,
-                        list: snapshot.data!.cities!,
-                        onChange: (dynamic val) => setState(() {
-                          profileBloc.userInfo.address!.cityId = val.id;
-                          print('city $val.id');
-                        }),
-                        border: true,
-                      );
-                    } else {
-                      return Container();
-                    }
-                  },
-                )),
+              textDirection: TextDirection.rtl,
+              child: StreamBuilder(
+                stream: profileBloc.cityProvinceModelStream,
+                builder: (context, AsyncSnapshot<CityProvinceModel> snapshot) {
+                  if (snapshot.data != null &&
+                      profileBloc.userInfo.address != null) {
+                    return MyDropdown(
+                      value: profileBloc.findCityName(),
+                      alignment: Alignment.centerRight,
+                      hint: intl.city,
+                      list: snapshot.data!.cities!,
+                      onChange: (dynamic val) => setState(() {
+                        profileBloc.userInfo.address!.cityId = val.id;
+                        print('city $val.id');
+                      }),
+                      border: true,
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ),
         ],
       ),
     );
