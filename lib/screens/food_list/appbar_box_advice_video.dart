b8cefdd25eeb0f0f1c7920d8cfae009dc311497b
mahdi eyvazi <mr.mahdi730@gmail.com>
2022-05-30 12:54:51 +0430

    fix show message when advice video not available

--- lib/screens/food_list/appbar_box_advice_video.dart
+++ lib/screens/food_list/appbar_box_advice_video.dart
@@ -19,7 +19,8 @@ class AppbarBoxAdviceVideo extends StatefulWidget {
   _AppbarBoxAdviceVideoState createState() => _AppbarBoxAdviceVideoState();
 }
 
-class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> {
+class _AppbarBoxAdviceVideoState
+    extends ResourcefulState<AppbarBoxAdviceVideo> {
   late FoodListBloc bloc;
 
   // int? _selectedDayIndex;
@@ -87,7 +88,8 @@ class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> 
                                   textDirection: context.textDirectionOfLocale,
                                   maxLines: 1,
                                   style: typography.overline!.copyWith(
-                                      fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
+                                      fontWeight: FontWeight.bold,
+                                      overflow: TextOverflow.ellipsis),
                                 ),
                               ],
                             )),
@@ -98,7 +100,11 @@ class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> 
                           ],
                         );
                     }
-                    return Center(child: EmptyBox());
+                    return Center(
+                        child: Text(
+                      intl.noAdviceVideo,
+                      style: typography.caption,
+                    ));
                   },
                 );
               }
@@ -154,7 +160,8 @@ class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> 
             ],
           )),
       onTap: () {
-        VxNavigator.of(context).push(Uri.parse(Routes.dailyMessage), params: bloc.adviceId);
+        VxNavigator.of(context)
+            .push(Uri.parse(Routes.dailyMessage), params: bloc.adviceId);
       },
     );
   }
@@ -171,7 +178,10 @@ class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> 
         child: Center(
           child: Text(
             intl.goToToday,
-            style: typography.caption!.copyWith(color: AppColors.primary,fontWeight: FontWeight.w700,),
+            style: typography.caption!.copyWith(
+              color: AppColors.primary,
+              fontWeight: FontWeight.w700,
+            ),
             softWrap: true,
           ),
         ),
@@ -183,7 +193,8 @@ class _AppbarBoxAdviceVideoState extends ResourcefulState<AppbarBoxAdviceVideo> 
   }
 
   bool isAfterToday(WeekDay day) {
-    return day.gregorianDate.isAfter(DateTime.parse(DateTime.now().toString().substring(0, 10)));
+    return day.gregorianDate
+        .isAfter(DateTime.parse(DateTime.now().toString().substring(0, 10)));
   }
 
   @override
