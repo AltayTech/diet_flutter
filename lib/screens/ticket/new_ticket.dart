06cc3070354d964d390b1763c49de9fb7ca9f22b
Mahdi <Mahdimoaz@gmail.com>
2022-07-20 15:18:03 +0430

    fix

--- lib/screens/ticket/new_ticket.dart
+++ lib/screens/ticket/new_ticket.dart
@@ -1,6 +1,7 @@
 import 'package:behandam/base/resourceful_state.dart';
 import 'package:behandam/base/utils.dart';
 import 'package:behandam/data/entity/ticket/ticket_item.dart';
+import 'package:behandam/extensions/color.dart';
 import 'package:behandam/screens/ticket/ticket_bloc.dart';
 import 'package:behandam/screens/ticket/ticket_provider.dart';
 import 'package:behandam/screens/ticket/ticket_type_button.dart';
@@ -63,41 +64,62 @@ class _NewTicketState extends ResourcefulState<NewTicket> {
             backgroundColor: AppColors.newBackground,
             body: SafeArea(
               child: SingleChildScrollView(
-                child: Column(children: [
-                  Container(
-                    transform: Matrix4.translationValues(0.0, -10.0, 0.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        topRight: Radius.circular(16),
-                      ),
+                child: Container(
+                  transform: Matrix4.translationValues(0.0, -10.0, 0.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
                     ),
-                    padding: EdgeInsets.symmetric(horizontal: 5.w),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Space(height: 4.h),
-                        Text(
-                          intl.titleQuestionTicket,
-                          style: Theme.of(context).textTheme.caption,
-                          textAlign: TextAlign.center,
-                        ),
-                        Space(height: 2.h),
-                        StreamBuilder(
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Space(height: 4.h),
+                      Text(
+                        intl.titleQuestionTicket,
+                        style: Theme.of(context).textTheme.caption,
+                        textAlign: TextAlign.center,
+                      ),
+                      Space(height: 2.h),
+                      Container(
+                        color: Color(0xfff0f5fe),
+                        width: 100.w,
+                        height: 8.h,
+                        padding: EdgeInsets.only(left: 5.w, right: 5.w),
+                        child: StreamBuilder(
                           builder: (context, snapshot) {
                             if (snapshot.data != null && snapshot.data == false) {
-                              return Wrap(
-                                spacing: 2.w,
-                                runSpacing: 1.h,
-                                textDirection: context.textDirectionOfLocale,
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                direction: Axis.horizontal,
-                                alignment: WrapAlignment.center,
-                                runAlignment: WrapAlignment.start,
-                                children: [
-                                  ...bloc.SupportItems.map((item) => supportTypeItem(item)).toList()
-                                ],
+                              return ShaderMask(
+                                shaderCallback: (Rect rect) {
+                                  return LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                    colors: [
+                                      Colors.black,
+                                      Colors.transparent,
+                                      Colors.transparent,
+                                      Colors.black
+                                    ],
+                                    stops: [
+                                      0.0,
+                                      0.05,
+                                      0.95,
+                                      1.0
+                                    ], // 10% purple, 80% transparent, 10% purple
+                                  ).createShader(rect);
+                                },
+                                blendMode: BlendMode.dstOut,
+                                child: ListView.builder(
+                                  itemBuilder: (context, index) {
+                                    return supportTypeItem(bloc.SupportItems[index]);
+                                  },
+                                  shrinkWrap: true,
+                                  reverse: false,
+                                  itemCount: bloc.SupportItems.length,
+                                  scrollDirection: Axis.horizontal,
+                                ),
                               );
                             } else {
                               return Center(
@@ -110,72 +132,79 @@ class _NewTicketState extends ResourcefulState<NewTicket> {
                           },
                           stream: bloc.progressNetwork,
                         ),
-                        Space(height: 2.h),
-                        textInput(
-                            height: 9.h,
-                            label: intl.subject,
-                            bgColor: Colors.white,
-                            value: bloc.sendTicketMessage.title ?? '',
-                            textController: ticketTitleController,
-                            validation: (validation) {},
-                            onChanged: (onChanged) {
-                              bloc.sendTicketMessage.title = onChanged;
-                            },
-                            maxLine: true,
-                            enable: true,
-                            ctx: context,
-                            textDirection: context.textDirectionOfLocale),
-                        Space(height: 2.h),
-                        Container(
-                          height: 24.h,
-                          child: Directionality(
-                            textDirection: context.textDirectionOfLocale,
-                            child: StreamBuilder(
-                              builder: (context, AsyncSnapshot<bool> snapshot) {
-                                return textInput(
-                                    height: 6.h,
-                                    label: intl.lableTextMessage,
-                                    textController: ticketDescController,
-                                    validation: (validation) {},
-                                    bgColor: Colors.white,
-                                    onChanged: (onChanged) {
-                                      bloc.sendTicketMessage.body = onChanged;
-                                    },
-                                    enable: !(snapshot.data ?? false),
-                                    maxLine: true,
-                                    ctx: context,
-                                    textDirection: context.textDirectionOfLocale,
-                                );
-                              },
-                              stream: bloc.isShowRecorder,
+                      ),
+                      Space(height: 2.h),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5.w),
+                        child: Column(
+                          children: [
+                            textInput(
+                                height: 9.h,
+                                label: intl.subject,
+                                bgColor: Colors.white,
+                                value: bloc.sendTicketMessage.title ?? '',
+                                textController: ticketTitleController,
+                                validation: (validation) {},
+                                onChanged: (onChanged) {
+                                  bloc.sendTicketMessage.title = onChanged;
+                                },
+                                maxLine: true,
+                                enable: true,
+                                ctx: context,
+                                textDirection: context.textDirectionOfLocale),
+                            Space(height: 2.h),
+                            Container(
+                              height: 24.h,
+                              child: Directionality(
+                                textDirection: context.textDirectionOfLocale,
+                                child: StreamBuilder(
+                                  builder: (context, AsyncSnapshot<bool> snapshot) {
+                                    return textInput(
+                                      height: 6.h,
+                                      label: intl.lableTextMessage,
+                                      textController: ticketDescController,
+                                      validation: (validation) {},
+                                      bgColor: Colors.white,
+                                      onChanged: (onChanged) {
+                                        bloc.sendTicketMessage.body = onChanged;
+                                      },
+                                      enable: !(snapshot.data ?? false),
+                                      maxLine: true,
+                                      ctx: context,
+                                      textDirection: context.textDirectionOfLocale,
+                                    );
+                                  },
+                                  stream: bloc.isShowRecorder,
+                                ),
+                              ),
                             ),
-                          ),
+                            TicketTypeButton(),
+                            Space(height: 3.h),
+                            StreamBuilder(
+                                stream: bloc.isShowProgressItem,
+                                builder: (context, snapshot) {
+                                  if (snapshot.data == null || snapshot.data == false)
+                                    return Padding(
+                                        padding: EdgeInsets.only(left: 12, right: 12),
+                                        child: SubmitButton(
+                                          onTap: checkTypeTicketForSend,
+                                          label: intl.sendMessage,
+                                        ));
+                                  else {
+                                    return Center(
+                                      child: SpinKitCircle(
+                                        size: 5.h,
+                                        color: AppColors.primary,
+                                      ),
+                                    );
+                                  }
+                                })
+                          ],
                         ),
-                        TicketTypeButton(),
-                        Space(height: 3.h),
-                        StreamBuilder(
-                            stream: bloc.isShowProgressItem,
-                            builder: (context, snapshot) {
-                              if (snapshot.data == null || snapshot.data == false)
-                                return Padding(
-                                    padding: EdgeInsets.only(left: 12, right: 12),
-                                    child: SubmitButton(
-                                      onTap: checkTypeTicketForSend,
-                                      label: intl.sendMessage,
-                                    ));
-                              else {
-                                return Center(
-                                  child: SpinKitCircle(
-                                    size: 5.h,
-                                    color: AppColors.primary,
-                                  ),
-                                );
-                              }
-                            })
-                      ],
-                    ),
-                  )
-                ]),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
@@ -202,55 +231,58 @@ class _NewTicketState extends ResourcefulState<NewTicket> {
     return StreamBuilder<bool>(
         stream: bloc.supportItemSelected,
         builder: (context, snapshot) {
-          return InkWell(
-            onTap: () {
-              for (SupportItem s in bloc.SupportItems) s.selected = false;
-              supportItem.selected = true;
-              bloc.sendTicketMessage.departmentId = supportItem.id;
-              bloc.setSupportItemSelected();
-            },
-            child: Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: supportItem.isSelected ? Border.all(color: AppColors.primary) : null,
-                borderRadius: AppBorderRadius.borderRadiusExtraLarge,
-              ),
-              constraints: BoxConstraints(minHeight: 6.h, maxWidth: 43.w),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  supportItem.isSelected
-                      ? ImageUtils.fromString(
-                          """<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="8.99976" cy="9" r="9" fill="#1ABC9C" />
-                  <path
-                  d="M12.848 4.17668C12.9324 4.15083 13.0318 4.17265 13.0956 4.23481C13.5696 4.70928 14.0439 5.18328 14.5181 5.65752C14.6137 5.74861 14.6168 5.9199 14.5212 6.01195C11.9344 8.59783 9.34897 11.1849 6.7619 13.7705C6.65799 13.8683 6.47698 13.8472 6.39157 13.735C5.33611 12.6785 4.27993 11.6226 3.22375 10.5667C3.12838 10.4744 3.13171 10.304 3.2266 10.2127C3.70178 9.73799 4.1765 9.26304 4.65121 8.78809C4.74325 8.69937 4.90363 8.69889 4.99662 8.78691C5.47299 9.26233 5.94865 9.73894 6.42479 10.2148C6.51494 10.3043 6.67412 10.3045 6.76475 10.2155C8.73002 8.25028 10.6953 6.28477 12.6606 4.31974C12.717 4.26541 12.7678 4.19614 12.848 4.17668Z"
-                  fill="white" />
-                  </svg>
-                      """
-                              .replaceAll('#1ABC9C', "#ff5757"),
-                          width: 3.w,
-                          height: 3.h,
-                        )
-                      : ImageUtils.fromLocal(
-                          "assets/images/bill/not_select.svg",
-                          width: 3.w,
-                          height: 3.h,
-                        ),
-                  Space(
-                    width: 2.w,
-                  ),
-                  Text(
-                    supportItem.displayName ?? "",
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.start,
-                    style: typography.caption,
-                  ),
-                ],
+          return Padding(
+            padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+            child: InkWell(
+              onTap: () {
+                for (SupportItem s in bloc.SupportItems) s.selected = false;
+                supportItem.selected = true;
+                bloc.sendTicketMessage.departmentId = supportItem.id;
+                bloc.setSupportItemSelected();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: supportItem.isSelected ? Border.all(color: AppColors.primary) : null,
+                  borderRadius: AppBorderRadius.borderRadiusExtraLarge,
+                ),
+                padding: EdgeInsets.only(left: 8, right: 8),
+                constraints: BoxConstraints(minWidth: 25.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    supportItem.isSelected
+                        ? ImageUtils.fromString(
+                            """<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="8.99976" cy="9" r="9" fill="#1ABC9C" />
+                    <path
+                    d="M12.848 4.17668C12.9324 4.15083 13.0318 4.17265 13.0956 4.23481C13.5696 4.70928 14.0439 5.18328 14.5181 5.65752C14.6137 5.74861 14.6168 5.9199 14.5212 6.01195C11.9344 8.59783 9.34897 11.1849 6.7619 13.7705C6.65799 13.8683 6.47698 13.8472 6.39157 13.735C5.33611 12.6785 4.27993 11.6226 3.22375 10.5667C3.12838 10.4744 3.13171 10.304 3.2266 10.2127C3.70178 9.73799 4.1765 9.26304 4.65121 8.78809C4.74325 8.69937 4.90363 8.69889 4.99662 8.78691C5.47299 9.26233 5.94865 9.73894 6.42479 10.2148C6.51494 10.3043 6.67412 10.3045 6.76475 10.2155C8.73002 8.25028 10.6953 6.28477 12.6606 4.31974C12.717 4.26541 12.7678 4.19614 12.848 4.17668Z"
+                    fill="white" />
+                    </svg>
+                        """
+                                .replaceAll('#1ABC9C', AppColors.primary.toHex()),
+                            width: 3.w,
+                            height: 3.h,
+                          )
+                        : ImageUtils.fromLocal(
+                            "assets/images/bill/not_select.svg",
+                            width: 3.w,
+                            height: 3.h,
+                          ),
+                    Space(
+                      width: 2.w,
+                    ),
+                    Text(
+                      supportItem.ticketName ?? "",
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.start,
+                      style: typography.overline,
+                    ),
+                  ],
+                ),
               ),
             ),
           );
