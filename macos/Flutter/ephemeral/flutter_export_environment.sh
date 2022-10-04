50bf3e5dc725e36efba0df307ea7ca04c5560de1
MahdiEyvazi <mr.mahdi730@gmail.com>
2022-10-04 16:11:40 +0330

    gitflow-feature-stash: linux_build

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter\behandam"
+export "FLUTTER_ROOT=/home/mahdi/flutterSdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mahdi/flutterProject/new-behandam-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=3.6.8"
 export "FLUTTER_BUILD_NUMBER=368"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
+export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
