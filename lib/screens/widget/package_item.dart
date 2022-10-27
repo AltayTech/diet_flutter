488d5825d318d68c4e46449f2a0e15a6cf632f74
MahdiEyvazi <mr.mahdi730@gmail.com>
2022-10-27 11:23:38 +0330

    fix package item ui

--- lib/screens/widget/package_item.dart
+++ lib/screens/widget/package_item.dart
@@ -277,56 +277,58 @@ class PackageWidget extends StatelessWidget {
                     Space(
                       height: 1.h,
                     ),
-                    Container(
-                      width: double.maxFinite,
-                      child: Row(
-                        children: [
-                          RichText(
-                            softWrap: true,
-                            textAlign: TextAlign.start,
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                    text: '${context.intl.toman}',
-                                    style: typography!.caption!.copyWith(
-                                        letterSpacing: -0.02,
-                                        color: Color(0xff454545),
-                                        fontSize: 9.sp,
-                                        fontWeight: FontWeight.w300))
-                              ],
-                              text: '$price'.seRagham() + '+ ',
-                              style: typography!.caption!.copyWith(
-                                  letterSpacing: -0.02,
-                                  color: Color(0xff454545),
-                                  fontSize: 14.sp,
-                                  fontWeight: FontWeight.w500),
+                    Expanded(
+                      child: Container(
+                        width: double.maxFinite,
+                        child: Row(
+                          children: [
+                            RichText(
+                              softWrap: true,
+                              textAlign: TextAlign.start,
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: '${context.intl.toman}',
+                                      style: typography!.caption!.copyWith(
+                                          letterSpacing: -0.02,
+                                          color: Color(0xff454545),
+                                          fontSize: 9.sp,
+                                          fontWeight: FontWeight.w300))
+                                ],
+                                text: '$price'.seRagham() + '+ ',
+                                style: typography!.caption!.copyWith(
+                                    letterSpacing: -0.02,
+                                    color: Color(0xff454545),
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ),
-                          ),
-                          description != null && description!.length > 0
-                              ? Expanded(
-                                  child: InkWell(
-                                    onTap: () {
-                                      serviceDescription(context, description!);
-                                    },
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          context.intl.moreDescription,
-                                          softWrap: true,
-                                          textAlign: TextAlign.start,
-                                          style: typography!.caption!.copyWith(
-                                              letterSpacing: -0.02,
-                                              color: Colors.red,
-                                              fontSize: 10.sp,
-                                              fontWeight: FontWeight.w500),
-                                        )
-                                      ],
+                            description != null && description!.length > 0
+                                ? Expanded(
+                                    child: InkWell(
+                                      onTap: () {
+                                        serviceDescription(context, description!);
+                                      },
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            context.intl.moreDescription,
+                                            softWrap: true,
+                                            textAlign: TextAlign.start,
+                                            style: typography!.caption!.copyWith(
+                                                letterSpacing: -0.02,
+                                                color: Colors.red,
+                                                fontSize: 10.sp,
+                                                fontWeight: FontWeight.w500),
+                                          )
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                )
-                              : EmptyBox(),
-                        ],
+                                  )
+                                : EmptyBox(),
+                          ],
+                        ),
                       ),
                     ),
                   ],
